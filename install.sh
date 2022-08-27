be46370093ebdbdfdc54e52e2da967d3e3fcf044
ezcafe <ezcafe@outlook.com>
2022-08-27 11:00:38 +0700

    Add powerlevel10k theme

--- install.sh
+++ install.sh
@@ -1,24 +1,44 @@
 #!/bin/sh
 
+git config --global user.name "Your Name"
+git config --global user.email you@example.com
+
+# Clone dotfiles
 cd ~
-git clone --recursive https://github.com/ezcafe/dotfiles.git ~
-
-# Backup
-mv ~/.bash_profile ~/.bash_profile-bk
-mv ~/.gitignore ~/.gitignore-bk
-mv ~/.zshrc ~/.zshrc-bk
-
-# Symlink configs
-ln -sv "~/dotfiles/bash/bash_profile" ~/.bash_profile
-ln -sv "~/dotfiles/git/gitignore" ~/.gitignore
-ln -sv "~/dotfiles/zsh/zshrc" ~/.zshrc
-
-# Source configs
-cd ~/dotfiles && source zsh/zshrc && ..
+git clone --recursive https://github.com/ezcafe/dotfiles.git
 
 # VSCode stuffs
 mkdir ~/Library/Application\ Support/Code/User
-cp ~/dotfiles/vscode/* ~/Library/Application\ Support/Code/User
+ln -sv ~/dotfiles/vscode/QDT.code-snippets ~/Library/Application\ Support/Code/User/snippets/QDT.code-snippets
+ln -sv ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 
 # Create ws folder
 mkdir ~/ws
+
+# Install zsh
+# https://github.com/ohmyzsh/ohmyzsh/wiki/Installing-ZSH#install-and-set-up-zsh-as-default
+# Install ohmyzsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Install font
+# https://github.com/romkatv/powerlevel10k#meslo-nerd-font-patched-for-powerlevel10k
+# Install powerlevel10k theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+# Install zsh-autosuggestions plugin
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+# Install zsh-syntax-highlighting/ plugin
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+# Update gitconfig + gitignore
+echo ~/dotfiles/git/gitconfig >> ~/.gitconfig
+echo ~/dotfiles/git/gitignore >> ~/.gitignore
+
+# Add this to .zshrc
+source ~/dotfiles/setup
+
+# Update
+# cd ~/dotfiles && gpr origin main
+# git -C ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k pull
+
+# Uninstall
+# powerlevel10k
+# https://github.com/romkatv/powerlevel10k#how-do-i-uninstall-powerlevel10k
