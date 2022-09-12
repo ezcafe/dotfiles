0951c2627e22f517c1f1899f8ada519d6cef04c5
Quang Pham <ezcafe@outlook.com>
2022-09-12 19:31:32 +0700

    feat(config): 🔥 update firefox theme

--- firefox/user.js
+++ firefox/user.js
@@ -1,1 +1,4 @@
+user_pref("gfx.webrender.all", true);
+user_pref("layers.acceleration.force-enabled", true);
+user_pref("svg.context-properties.content.enabled", true);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
